bde7a318f57a51017853ce84f06c7e0fcb10445e
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2022-10-24 21:14:20 -0300

    01

---
+++ app/src/main/java/com/example/helloworld/operacoes.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+public class operacoes {
+    private float a;
+    private float b;
+
+    public operacoes(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+    public float soma () {
+
+    }
+}
